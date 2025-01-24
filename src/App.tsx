2e1db753750da15a37c72002fa6a0be6dcc9d752
OskarMartens <martens.oskar@gmail.com>
2025-01-24 11:04:28 +0100

    Removed tests function for I18Next in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
 import "./App.css";
 
 function App() {
-    const {
-        t,
-        i18n: { changeLanguage, language }
-    } = useTranslation();
-    const [currentLanguage, setCurrentLanguage] = useState(language);
-
-    const handleChangeLanguage = () => {
-        const newLanguage = currentLanguage === "en" ? "sv" : "en";
-        setCurrentLanguage(newLanguage);
-        changeLanguage(newLanguage);
-    };
-
-    return (
-        <div className="App">
-            <h1>
-                Our Translated Header:
-                {t("headerTitle", { appName: "App for Translations" })}
-            </h1>
-            <h3>Current Language: {currentLanguage}</h3>
-            <button
-                type="button"
-                onClick={handleChangeLanguage}
-            >
-                Change Language
-            </button>
-        </div>
-    );
+    return(
+        <>Hello World!</>
+    )
 }
 
 export default App;
