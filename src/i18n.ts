77c44c5fdeaaf2513f02899ef2e982960095dd31
maxth <maxthorstorp@gmail.com>
2025-01-25 17:39:52 +0100

    apply prettier across the project to pass workflow

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enJSON from './locale/en.json'
-import svJSON from './locale/sv.json'
+import enJSON from "./locale/en.json";
+import svJSON from "./locale/sv.json";
 
 i18n.use(initReactI18next).init({
- resources: {
-	en: {...enJSON},
-	sv: {...svJSON}
- },
- lng: "en",     // Set the initial language of the App
+    resources: {
+        en: { ...enJSON },
+        sv: { ...svJSON }
+    },
+    lng: "en" // Set the initial language of the App
 });
