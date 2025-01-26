bee97ec9abdc73c76d2054121a2f866a70426010
maxth <maxthorstorp@gmail.com>
2025-01-26 05:59:10 +0100

    update eslint config according to readme boilerplate

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import enJSON from "./locale/en.json";
 import svJSON from "./locale/sv.json";
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18n.use(initReactI18next).init({
     resources: {
         en: { ...enJSON },
