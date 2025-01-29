33a64270b9792bbded401352395c39a6d9b7c58a
OskarMartens <martens.oskar@gmail.com>
2025-01-29 11:44:43 +0100

    Update QuestionHeader with props and css.

--- src/components/questionCard/questCardHeader/QuestionHeader.tsx
+++ src/components/questionCard/questCardHeader/QuestionHeader.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import styles from "./QuestionHeader.module.css";
+import check_circle_white from "../../../assets/icons/check_circle_white.svg";
 
-export function QuestionHeader() {
+interface QuestionHeaderProps {
+    data: {
+        courseName: string;
+        courseCode: string;
+        isProtected: boolean;
+        isResolved: boolean;
+        isHidden: boolean;
+    };
+}
+
+export function QuestionHeader({ data }: QuestionHeaderProps) {
+    const statusClass = data.isResolved ? styles.resolved : styles.notResolved;
+
     return (
         <div className={styles.container}>
-            <div>General</div>
-            <div>Test</div>
+            <h3 className={styles.courseTitle}>
+                {data.courseCode} - {data.courseName}
+            </h3>
+            <div className={`${styles.status} ${statusClass}`}>
+                {data.isResolved && (
+                    <img
+                        src={check_circle_white}
+                        alt="Resolved"
+                        className={styles.icon}
+                    />
+                )}
+                <span>{data.isResolved ? "Resolved" : "Not Resolved"}</span>
+            </div>
         </div>
     );
 }
