6dc1d8b36d410694631543059444682cc38f098e
OskarMartens <martens.oskar@gmail.com>
2025-01-29 11:42:45 +0100

    Add dummy data to HomeLimited and update HomeLimited css.

--- src/pages/homeLimited/HomeLimited.tsx
+++ src/pages/homeLimited/HomeLimited.tsx
@@ -1,12 +1,12 @@
 import { QuestionCard } from "../../components";
-import styles from './HomeLimited.module.css';
+import styles from "./HomeLimited.module.css";
+
+import dummy_question from "../../data/json/dummy_question.json";
 
 export function HomeLimited() {
     return (
         <div className={styles.card_container}>
-
-            <QuestionCard />
-
+            <QuestionCard data={dummy_question} />
         </div>
     );
 }
