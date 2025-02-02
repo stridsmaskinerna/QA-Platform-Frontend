4995543be0868c21c931a3e9379e61a40ac47568
maxth <maxthorstorp@gmail.com>
2025-02-02 14:51:34 +0100

    finished with search, category and topic filters. isResolved filter remains

--- src/components/search/searchWithFilters/SearchWithFilters.tsx
+++ src/components/search/searchWithFilters/SearchWithFilters.tsx
@@ -6,6 +6,8 @@ import styles from "./SearchWithFilters.module.css";
 
 export function SearchWithFilters(props: ISearchWithFiltersProps) {
     const { t } = useTranslation();
+    const showTopics =
+        props.subjectFilter.activeFilter && props.showTopicsFilters;
     return (
         <div className={styles.container}>
             <SearchBar
@@ -13,15 +15,15 @@ export function SearchWithFilters(props: ISearchWithFiltersProps) {
                 placeholder={props.placeholder}
                 onInputChange={props.onInputChange}
             />
-            {props.subjectFilter.displayedFilters.length && (
-                <SearchFilter
-                    title={`${t("categoryFilter")}:`}
-                    onFilterClick={props.subjectFilter.onFilterClick}
-                    displayedFilters={props.subjectFilter.displayedFilters}
-                    activeFilter={props.subjectFilter.activeFilter}
-                />
-            )}
-            {props.subjectFilter.activeFilter && props.showTopicsFilters && (
+
+            <SearchFilter
+                title={`${t("categoryFilter")}:`}
+                onFilterClick={props.subjectFilter.onFilterClick}
+                displayedFilters={props.subjectFilter.displayedFilters}
+                activeFilter={props.subjectFilter.activeFilter}
+            />
+
+            {showTopics && (
                 <SearchFilter
                     title={`${t("topicFilter")}:`}
                     onFilterClick={props.topicFilter.onFilterClick}
