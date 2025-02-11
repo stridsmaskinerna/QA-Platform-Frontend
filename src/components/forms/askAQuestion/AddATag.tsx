97eb6f05d16de7f574558d97aabdc40d062a860b
maxth <maxthorstorp@gmail.com>
2025-02-11 15:27:07 +0100

    add a tag functionality finished

--- src/components/forms/askAQuestion/AddATag.tsx
+++ src/components/forms/askAQuestion/AddATag.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import styles from "./AddATag.module.css";
-import { InputWithDynamicSuggestions } from "../..";
+import { InputWithDynamicSuggestions, Modal } from "../..";
 import {
     ChangeEventHandler,
     useCallback,
@@ -12,6 +12,7 @@ import { useFetchWithToken } from "../../../hooks";
 import { BASE_URL } from "../../../data";
 import { ISuggestion, ITag } from "../../../utils";
 import { useDebounceCallback } from "usehooks-ts";
+import removeX from "../../../assets/icons/removeX.svg";
 
 const url = `${BASE_URL}/tags`;
 
@@ -32,6 +33,7 @@ export function AddATag({
     const [displayedSuggestions, setDisplayedSuggestions] = useState<
         ISuggestion[]
     >([]);
+    const [showWarning, setShowWarning] = useState(false);
     const hideSuggestionsTimeoutRef = useRef<NodeJS.Timeout | null>(null);
     const onInputChange: ChangeEventHandler<HTMLInputElement> = e => {
         e.preventDefault();
@@ -60,6 +62,20 @@ export function AddATag({
         300,
     );
 
+    const handleAddClick = () => {
+        if (addedTags.includes(inputValue.trim().toUpperCase())) {
+            return;
+        }
+
+        if (addedTags.length === 5) {
+            setShowWarning(true);
+            return;
+        }
+        onAddClick(inputValue.trim().toUpperCase());
+        setInputValue("");
+        setDisplayedSuggestions([]);
+    };
+
     const onSuggestionClick = ({ name }: ISuggestion) => {
         setInputValue(name);
     };
@@ -77,30 +93,50 @@ export function AddATag({
     }, []);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.infoContainer}>
-                <h5>{t("tags")}</h5>
-                <p>{t("addATagExpl")}</p>
-            </div>
-            <div className={styles.inputContainer}>
-                <InputWithDynamicSuggestions
-                    inputValue={inputValue}
-                    onChange={onInputChange}
-                    placeHolder={t("addATag")}
-                    onSuggestionClick={onSuggestionClick}
-                    displayedSuggestions={displayedSuggestions}
-                    inputType="search"
-                    onFocus={onFocus}
-                />
+        <>
+            <div className={styles.container}>
+                <div className={styles.infoContainer}>
+                    <h5>{t("tags")}</h5>
+                    <p>{t("addATagExpl")}</p>
+                </div>
+                <div className={styles.inputContainer}>
+                    <InputWithDynamicSuggestions
+                        inputValue={inputValue}
+                        onChange={onInputChange}
+                        placeHolder={t("addATag")}
+                        onSuggestionClick={onSuggestionClick}
+                        displayedSuggestions={displayedSuggestions}
+                        inputType="search"
+                        onFocus={onFocus}
+                    />
 
-                <button
-                    onClick={() => onAddClick(inputValue)}
-                    className={styles.addBtn}
-                    type="button"
-                >
-                    {t("add")}
-                </button>
+                    <button
+                        onClick={handleAddClick}
+                        className={styles.addBtn}
+                        type="button"
+                    >
+                        {t("add")}
+                    </button>
+                </div>
+                <div className={styles.addedTagsContainer}>
+                    {addedTags.map(t => (
+                        <button
+                            className={styles.addedTag}
+                            onClick={() => onRemoveClick(t)}
+                            key={t}
+                        >
+                            <p>{t}</p>
+                            <img src={removeX} />
+                        </button>
+                    ))}
+                </div>
             </div>
-        </div>
+            {showWarning && (
+                <Modal
+                    okClick={() => setShowWarning(false)}
+                    message={t("tagLimitWarning")}
+                />
+            )}
+        </>
     );
 }
