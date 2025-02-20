ffb730091b0f58d9ac3c75b3c6f41202ff55acca
maxth <maxthorstorp@gmail.com>
2025-02-20 16:29:18 +0100

    impl hide-show on QuestionCardDetails

--- src/components/questionCard/questionCardDetails/QuestionCardDetails.tsx
+++ src/components/questionCard/questionCardDetails/QuestionCardDetails.tsx
@@ -1,25 +1,34 @@
+import { useState } from "react";
 import { QuestionHeader, QuestionCardMiddle, QuestionCardBottom } from "..";
 import { IQuestion } from "../../../utils";
 import styles from "../QuestionCard.module.css";
 
 export function QuestionCardDetails(data: IQuestion) {
+    const [isHiddenOptimistic, setIsHiddenOptimistic] = useState(data.isHidden);
     return (
         <div className={styles.container}>
             <QuestionHeader
-                isHidden={data.isHidden}
+                setIsHiddenOptimistic={setIsHiddenOptimistic}
+                isHiddenOptimistic={isHiddenOptimistic}
+                isHideable={data.isHideable}
                 isProtected={data.isProtected}
                 isResolved={data.isResolved}
                 subjectCode={data.subjectCode}
                 subjectName={data.subjectName}
+                id={data.id}
             />
             <QuestionCardMiddle
+                isHiddenOptimistic={isHiddenOptimistic}
                 title={data.title}
                 created={data.created}
                 username={data.userName}
                 topicName={data.topicName}
                 description={data.description}
             />
-            <QuestionCardBottom tags={data.tags} />
+            <QuestionCardBottom
+                tags={data.tags}
+                isHiddenOptimistic={isHiddenOptimistic}
+            />
         </div>
     );
 }
