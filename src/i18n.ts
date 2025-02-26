6fee7dd0e328a79349bcfc5888a73b06e7cb3d2b
Joel Martelleur <joel.martelleur@gmail.com>
2025-02-26 21:40:04 +0100

    Update translations to be more sclable.

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,14 +2,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enDevJSON from "./locale/en/index";
 import svDevJSON from "./locale/sv/index";
-import enJSON from "./locale/en/index.json";
-import svJSON from "./locale/sv/index.json";
 
 const isDev = import.meta.env.MODE === "development";
 
 export const resources = {
-    en: isDev ? { ...enDevJSON } : { ...enJSON },
-    sv: isDev ? { ...svDevJSON } : { ...svJSON },
+    en: isDev ? { ...enDevJSON } : { ...enDevJSON },
+    sv: isDev ? { ...svDevJSON } : { ...svDevJSON },
 } as const;
 
 export const defaultNS = "translation";
